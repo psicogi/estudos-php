e4b9bdfdfa6dd24bd4c3d0d66f7a9673ff3c2986
Giovania Dantas <fabianagiovana321@gmail.com>
2026-09-17 16:38:07 -0300

    entendendo a sintaxe do if else

--- index.php
+++ index.php
@@ -13,17 +13,56 @@
         $nome = "gi";
         $titulo = $saudacao . " portifolio da " . $nome;
         $subtitulo = "Seja bem vindo ao meu portifolio";
+        $ano = 2026;
+        $projeto = "Portifolio";
+        $descricao = "Meu portifolio feito em HTML e PHP";
+        $dataProjeto = "2026-09-16";
+        $finalizado = true;
     ?>
 
 
-    <h1>
-        <?=$titulo?> <!-- maneira mais curta de imprimir variavel -->
-    </h1>
+    <h1> <?=$titulo?> <!-- maneira mais curta de imprimir variavel --> </h1>
     <p>
         <?php 
             echo $subtitulo;
         ?>
     </p>
+
+    <p>
+        <?php
+            echo $ano;
+        ?>
+    </p>
+
+    <hr>
+
+    <div>
+        <h2><?= $projeto?></h2>
+        <p>
+            <?php 
+                echo $descricao;
+            ?>
+        </p>
+        <div>
+            <div><?=$dataProjeto?></div>
+            <div> Projeto: 
+                <?php if ($finalizado): ?> <!--sintaxe mais curta do if else-->
+                    <span style="color: green;">finalizado ✅</span>
+                <?php else: ?>
+                    <span style="color: red;">não finalizado ❎</span>
+                <?php endif; ?>
+                <?php 
+
+                    if ($finalizado) {
+                        echo " finalizado ✅";
+                    } else {
+                        echo " não finalizado ❎";
+                    }
+                ?>
+            </div>
+        </div>
+
+    </div>
     
 </body>
 </html>
